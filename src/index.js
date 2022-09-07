5388a8160d850192957cd6664f515100f77af01c
Maxxbay <maxxbay@gmail.com>
2022-09-07 23:28:09 +0200

    summary homework1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-
-import 'modern-normalize/modern-normalize.css';
+import ReactDOM from 'react-dom';
+import App from 'components/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
